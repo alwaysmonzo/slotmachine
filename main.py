70036880ca3ebd53bac027b5569276cbcd1436fa
m o n z o <creator.shminty@gmail.com>
2022-10-07 21:56:33 +0100

    fixed deposit() , check for int didnt work bc str

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 def deposit():
     print('1 spin = $100')
     depositing = True
+
     while depositing:
         depAmount = input('How much money would you like to bet\n$')
-        if type(depAmount) == int:
+        if depAmount.isdigit():
             depAmount = int(depAmount)
             if depAmount > 100:
-                break
+                depositing = False
             else:
                 print('you need at least $100 to play')
         else:
