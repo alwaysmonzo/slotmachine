75452e42ab7f4595e19f86022f5bf77dd5069ca7
m o n z o <creator.shminty@gmail.com>
2022-10-08 21:34:57 +0100

    made a win check .. removed 3 functions bc issues

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import shuffle, random, choice
 from time import sleep
+from keyboard import is_pressed
 
-def deposit():
+
+'''def deposit():
     print('1 spin = $100')
     depositing = True
 
@@ -16,30 +18,30 @@ def deposit():
         else:
             print('please input an amount!')
     
-    return depAmount
+    return depAmount'''
     
 
-def jackpot():
+'''def jackpot():
     j = open('jackpot.txt', 'r+')
     jackpotValue = j.read()
     
-    return jackpotValue
+    return jackpotValue'''
 
 
-def spins():
+'''def spins():
     global depAmount
     costPerSpin = 100
-    noSpins = (depAmount // costPerSpin)
+    noSpins = (d // costPerSpin)
 
-    return noSpins
+    return noSpins'''
 
 
 def randomizer():
     return 1 - random()
     
-x = ['Y','V','T','R','W',7,'X','D','A','S']
-y = ['Y','V','T','R','W',7,'X','D','A','S']
-z = ['Y','V','T','R','W',7,'X','D','A','S']
+x = ['Y','V','T','7⭐','R']
+y = ['Y','V','T','7⭐','R']
+z = ['Y','V','T','7⭐','R']
 
 shuffle(x,randomizer)
 shuffle(y, randomizer)
@@ -53,9 +55,9 @@ def intervals(gap):
 def slotsGame():
     global x, y, z, game
 
-    playing = True
     rotations = 0
     gap = 0.1
+    playing = True
 
     while playing:
         for i in range(0,1):
@@ -67,24 +69,38 @@ def slotsGame():
             y.append(y.pop(y.index(y[i])))
             z.append(z.pop(z.index(z[i])))
 
-            print(f'\r{game}')
+            print(game)
             intervals(gap)
         rotations += 1
+        if rotations == 1:
+            gap = 0.15
         if rotations == 5:
             gap = 0.2
-        if rotations == 15:
+        if rotations == 10:
             gap = 0.25
-        if rotations == 25:
-            gap = 0.4
-        if rotations == 30:
+        if rotations == 15:
             print(game)
             break
+        
 
+
+def winCheck():
+    print('---------------------------')
+    if (game[0] == game[1] == game[2]):
+        print('YOU WIN!!')
+        print('---------------------------')
         
-        
+    else:
+        print('YOU LOSE')
+        print('---------------------------')
+
+
 
 slotsGame()
+winCheck()
+
     
+
 
 
 
